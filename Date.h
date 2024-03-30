023a44620e6b6c409a6eb19064197f7ac2232ef4
Coral Engel <81955555+coralen@users.noreply.github.com>
2024-03-30 11:39:42 +0300

    Add files via upload

---
+++ Date.h
@@ -0,0 +1,24 @@
+#ifndef _DATE_H
+#define _DATE_H
+
+#include "Helper.h"
+
+#define MIN_DAY 1
+#define MIN_MONTH 1
+#define MAX_MONTH 12
+#define MIN_YEAR 2023
+#define SEGMENTS 3
+#define CHARS_COUNT 12
+
+typedef struct {
+	int day;
+	int month;
+	int year;
+} Date;
+
+void getCorrectDate(Date* pDate);
+void printDate(const Date* pdate);
+int validateDate(const int day, const int month, const int year);
+
+
+#endif
