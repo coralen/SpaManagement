4666e42e279464052bbedd6a319bc3654b738426
coralen <coralosh12@gmail.com>
2024-04-02 22:05:29 +0300

    errors fixup

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
-#include "Helper.h"
 
+#include "Helper.h"
 #include "SpaManagement.h"
 
-int menu();
 
 int main()
 {
-	TreatmentManager tManager;
-	RoomManager rManager;
-	addTreatment(&tManager, &rManager);
-	return 1;
-}
-
-int menu()
-{
-	return 1;
+	//TreatmentManager tManager;
+	//RoomManager rManager;
+	//addTreatment(&tManager, &rManager);
+	return 0;
 }
